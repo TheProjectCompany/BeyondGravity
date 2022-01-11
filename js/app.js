54150414ec3da1121c04e935263b60b80c21bd7d
Prachi Pathak <pathakprachi2001@gmail.com>
2022-01-12 00:26:18 +0530

    Sidebar Added

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+    sidebar.classList.toggle("show-sidebar");
+    document.getElementById('toggleBtn').style.visibility="hidden";
+  });
+  
+  closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar");
+    
+        document.getElementById('toggleBtn').style.visibility="visible";
+    
+
+  });
